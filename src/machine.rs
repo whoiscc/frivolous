5199de1dddcb6ea26778a495cff1b0fca1132872
whoiscc <correctizer@gmail.com>
2024-05-31 03:29:21 +0000

    Revise memory interface

--- src/machine.rs
+++ src/machine.rs
@@ -1,3 +1,9 @@
+use std::{
+    cmp::Ordering::{Greater, Less},
+    collections::BTreeMap,
+    sync::Arc,
+};
+
 use crate::memory::{Address, Memory};
 
 pub type CodeId = usize;
@@ -10,7 +16,7 @@ pub enum Instruction {
     LoadBool(bool),
     LoadInt(i32),
     LoadString(String),
-    LoadCode(Box<Code>),
+    LoadCode(Box<Code>, Vec<StackOffset>),
     LoadLayout(Vec<String>),
     LoadType(StackOffset), // of layout
     LoadRecord(StackOffset, Vec<(String, StackOffset)>),
@@ -23,7 +29,6 @@ pub enum Instruction {
     TypeGet(StackOffset, String),
     TraitGet(StackOffset, Vec<StackOffset>, String), // (trait, impl types, name)
     Operator2(InstructionOperator2, StackOffset, StackOffset),
-    Call(StackOffset, Vec<StackOffset>),
     Match(StackOffset, StackOffset), // (matched, type)
 
     Set(StackOffset, StackOffset),
@@ -33,6 +38,8 @@ pub enum Instruction {
 
     Jump(InstructionOffset),
     JumpIf(StackOffset, InstructionOffset),
+
+    Call(StackOffset, Vec<StackOffset>),
     Return(StackOffset),
 }
 
@@ -52,12 +59,12 @@ pub enum InstructionOperator2 {
     GreaterEqual,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Code {
     pub hints: String,
     pub captures: Vec<Address>,
     pub num_parameter: usize,
-    pub instructions: Vec<Instruction>,
+    pub instructions: Arc<[Instruction]>,
 }
 
 #[derive(Debug, Default)]
@@ -70,6 +77,7 @@ pub struct Machine {
 struct Frame {
     code_address: Address,
     base_offset: StackOffset,
+    return_offset: StackOffset,
     instruction_offset: InstructionOffset,
 }
 
@@ -85,7 +93,7 @@ impl Machine {
     ) -> anyhow::Result<()> {
         anyhow::ensure!(self.stack.is_empty());
         anyhow::ensure!(self.frames.is_empty());
-        self.push_frame(code_address, Vec::new(), memory)?;
+        self.push_frame(code_address, Vec::new())?;
         self.evaluate(memory)
     }
 
@@ -93,49 +101,256 @@ impl Machine {
         &mut self,
         code_address: Address,
         arguments: Vec<Address>,
-        memory: &mut Memory,
     ) -> anyhow::Result<()> {
+        let Some(code) = code_address.get_any_ref()?.downcast_ref::<Code>() else {
+            anyhow::bail!("expecting Code object")
+        };
+        anyhow::ensure!(code.num_parameter == arguments.len());
+        let captures = code.captures.clone();
         let frame = Frame {
             code_address,
             base_offset: self.stack.len(),
+            return_offset: captures.len() + arguments.len(),
             instruction_offset: 0,
         };
-        let code = memory
-            .get_any_ref(frame.code_address)?
-            .downcast_ref::<Code>()
-            .ok_or(anyhow::format_err!("expecting Code object"))?;
-        anyhow::ensure!(code.num_parameter == arguments.len());
-        self.stack.extend(code.captures.clone());
+        self.frames.push(frame);
+        self.stack.extend(captures.clone());
         self.stack.extend(arguments);
         Ok(())
     }
 
     fn evaluate(&mut self, memory: &mut Memory) -> anyhow::Result<()> {
-        let frame = self
-            .frames
-            .last_mut()
-            .ok_or(anyhow::format_err!("evaluating frame is missing"))?;
-        let code = memory
-            .get_any_ref(frame.code_address)?
-            .downcast_ref::<Code>()
-            .ok_or(anyhow::format_err!("expecting Code object"))?;
-        for instruction in &code.instructions[frame.instruction_offset..] {
-            frame.instruction_offset += 1;
-            use Instruction::*;
-            match instruction {
-                Jump(offset) => {
-                    frame.instruction_offset = *offset;
-                    break;
-                }
-                JumpIf(i, offset) => {
-                    if memory.get_bool(self.stack[frame.base_offset + *i])? {
-                        frame.instruction_offset = *offset;
-                        break;
+        'nonlocal_jump: loop {
+            let frame = self
+                .frames
+                .last_mut()
+                .ok_or(anyhow::format_err!("evaluating frame is missing"))?;
+            let code = frame
+                .code_address
+                .get_any_ref()?
+                .downcast_ref::<Code>()
+                .ok_or(anyhow::format_err!("expecting Code object"))?;
+            'local_jump: loop {
+                for instruction in &code.instructions[frame.instruction_offset..] {
+                    frame.instruction_offset += 1;
+                    use Instruction::*;
+                    match instruction {
+                        Jump(offset) => {
+                            frame.instruction_offset = *offset;
+                            continue 'local_jump;
+                        }
+                        JumpIf(i, offset) => {
+                            if self.stack[frame.base_offset + *i].get_bool()? {
+                                frame.instruction_offset = *offset;
+                                continue 'local_jump;
+                            }
+                        }
+                        Call(code_i, args_i) => {
+                            let code_address = self.stack[frame.base_offset + *code_i].clone();
+                            let arguments = args_i
+                                .iter()
+                                .map(|i| self.stack[frame.base_offset + *i].clone())
+                                .collect();
+                            frame.return_offset = self.stack.len();
+                            self.push_frame(code_address, arguments)?;
+                            continue 'nonlocal_jump;
+                        }
+                        Return(i) => {
+                            let address = self.stack[frame.base_offset + *i].clone();
+                            self.frames.pop();
+                            if let Some(frame) = self.frames.last_mut() {
+                                self.stack.truncate(frame.return_offset);
+                                self.stack.push(address);
+                                continue 'nonlocal_jump;
+                            } else {
+                                self.stack[0] = address;
+                                return Ok(());
+                            };
+                        }
+
+                        Rewind(i) => {
+                            anyhow::ensure!(self.stack.len() > frame.base_offset + *i);
+                            let address = self.stack.last().unwrap().clone();
+                            // `splice` probably can do but less readable
+                            self.stack.truncate(frame.base_offset + *i);
+                            self.stack.push(address)
+                        }
+
+                        LoadUnit => self.stack.push(memory.allocate_unit()),
+                        LoadBool(b) => self.stack.push(memory.allocate_bool(*b)),
+                        LoadInt(int) => self.stack.push(memory.allocate_int(*int)),
+                        LoadString(string) => self
+                            .stack
+                            .push(memory.allocate_any(Box::new(string.clone()))),
+                        LoadCode(code, captures) => {
+                            anyhow::ensure!(code.captures.is_empty());
+                            let mut code = code.clone();
+                            code.captures = captures
+                                .iter()
+                                .map(|i| self.stack[frame.base_offset + *i].clone())
+                                .collect();
+                            self.stack.push(memory.allocate_any(code))
+                        }
+                        LoadLayout(layout) => self
+                            .stack
+                            .push(memory.allocate_any(Box::new(RecordLayout(layout.clone())))),
+                        LoadType(layout_i) => {
+                            let t = Type {
+                                layout: self.stack[frame.base_offset + *layout_i].clone(),
+                                attributes: Default::default(),
+                            };
+                            self.stack.push(memory.allocate_any(Box::new(t)))
+                        }
+                        LoadRecord(type_i, fields) => {
+                            let Some(t) = self.stack[frame.base_offset + *type_i]
+                                .get_any_ref()?
+                                .downcast_ref::<Type>()
+                            else {
+                                anyhow::bail!("expecting type object")
+                            };
+                            let Some(RecordLayout(layout)) = t.layout.get_any_ref()?.downcast_ref()
+                            else {
+                                anyhow::bail!("type layout is not a Layout object")
+                            };
+                            let mut addresses = Vec::new();
+                            for name in layout {
+                                let i = fields
+                                    .iter()
+                                    .find_map(
+                                        |(other_name, i)| {
+                                            if other_name == name {
+                                                Some(*i)
+                                            } else {
+                                                None
+                                            }
+                                        },
+                                    )
+                                    .ok_or(anyhow::format_err!(
+                                        "missing field {name} in record initialization"
+                                    ))?;
+                                addresses.push(self.stack[frame.base_offset + i].clone())
+                            }
+                            let record = Record {
+                                layout: t.layout.clone(),
+                                addresses,
+                            };
+                            self.stack.push(memory.allocate_any(Box::new(record)))
+                        }
+                        LoadTrait(layout) => {
+                            let t = Trait {
+                                layout: layout.clone(),
+                                implementations: Default::default(),
+                            };
+                            self.stack.push(memory.allocate_any(Box::new(t)))
+                        }
+
+                        Set(i, source_i) => {
+                            match i.cmp(source_i) {
+                                Less => {
+                                    let (stack, remaining_stack) = self.stack.split_at_mut(*i + 1);
+                                    stack
+                                        .last_mut()
+                                        .unwrap()
+                                        .copy_from(&remaining_stack[source_i - i - 1])?
+                                }
+                                Greater => {
+                                    let (stack, remaining_stack) =
+                                        self.stack.split_at_mut(*source_i + 1);
+                                    remaining_stack[i - source_i - 1]
+                                        .copy_from(stack.last().unwrap())?
+                                }
+                                _ => {} // would be some silly case like `set x = x`
+                            }
+                        }
+                        RecordGet(i, name) => {
+                            let Some(record) = self.stack[frame.base_offset + *i]
+                                .get_any_ref()?
+                                .downcast_ref::<Record>()
+                            else {
+                                anyhow::bail!("expecting record object")
+                            };
+                            let Some(RecordLayout(layout)) =
+                                record.layout.get_any_ref()?.downcast_ref()
+                            else {
+                                anyhow::bail!("record layout is not a Layout object")
+                            };
+                            let p = layout
+                                .iter()
+                                .position(|other_name| other_name == name)
+                                .ok_or(anyhow::format_err!("unexpected field name {name}"))?;
+                            self.stack.push(record.addresses[p].clone())
+                        }
+                        RecordSet(i, name, j) => {
+                            let address = self.stack[frame.base_offset + *j].clone();
+                            let Some(record) = self.stack[frame.base_offset + *i]
+                                .get_any_mut()?
+                                .downcast_mut::<Record>()
+                            else {
+                                anyhow::bail!("expecting record object")
+                            };
+                            let Some(RecordLayout(layout)) =
+                                record.layout.get_any_ref()?.downcast_ref()
+                            else {
+                                anyhow::bail!("record layout is not a Layout object")
+                            };
+                            let p = layout
+                                .iter()
+                                .position(|other_name| other_name == name)
+                                .ok_or(anyhow::format_err!("unexpected field name {name}"))?;
+                            record.addresses[p] = address
+                        }
+                        TypeGet(i, name) => {
+                            let Some(t) = self.stack[frame.base_offset + *i]
+                                .get_any_ref()?
+                                .downcast_ref::<Type>()
+                            else {
+                                anyhow::bail!("expecting type object")
+                            };
+                            let Some(address) = t.attributes.get(name) else {
+                                anyhow::bail!("there is no attribute {name} on type object")
+                            };
+                            self.stack.push(address.clone())
+                        }
+                        TypeSet(i, name, j) => {
+                            let address = self.stack[frame.base_offset + *j].clone();
+                            let Some(t) = self.stack[frame.base_offset + *i]
+                                .get_any_mut()?
+                                .downcast_mut::<Type>()
+                            else {
+                                anyhow::bail!("expecting type object")
+                            };
+                            let replaced = t.attributes.insert(name.clone(), address);
+                            anyhow::ensure!(
+                                replaced.is_none(),
+                                "duplicated setting attribute {name} on type object"
+                            )
+                        }
+                        _ => todo!(),
                     }
                 }
-                _ => todo!(),
+                anyhow::bail!("Code instructions not end with Return")
             }
         }
-        Ok(())
     }
+}
+
+#[derive(Debug)]
+struct Record {
+    layout: Address,
+    addresses: Vec<Address>,
+}
+
+#[derive(Debug)]
+struct RecordLayout(Vec<String>);
+
+#[derive(Debug)]
+struct Type {
+    layout: Address,
+    attributes: BTreeMap<String, Address>,
+}
+
+#[derive(Debug)]
+struct Trait {
+    layout: Vec<String>,
+    implementations: BTreeMap<Vec<Address>, Vec<Address>>,
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,8 @@
 use std::any::Any;
 
-pub type Address = u64;
+// not impl Copy and Default intentionally
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Address(u64);
 
 #[derive(Debug, Default)]
 pub struct Memory {
@@ -14,42 +16,45 @@ impl Memory {
     }
 
     pub fn allocate_unit(&mut self) -> Address {
-        0
+        Address(0)
     }
 
     pub fn allocate_bool(&mut self, b: bool) -> Address {
-        0
+        Address(0)
     }
 
     pub fn allocate_int(&mut self, int: i32) -> Address {
-        0
+        Address(0)
     }
 
     pub fn allocate_any(&mut self, any: Box<dyn Any>) -> Address {
-        0
+        Address(0)
     }
+}
 
-    pub fn copy(&mut self, address: Address, source_address: Address) -> anyhow::Result<()> {
+impl Address {
+    #[allow(unused_variables)]
+    pub fn copy_from(&mut self, source_address: &Address) -> anyhow::Result<()> {
         Ok(())
     }
 
-    pub fn get_unit(&self, address: Address) -> anyhow::Result<()> {
+    pub fn get_unit(&self) -> anyhow::Result<()> {
         Ok(())
     }
 
-    pub fn get_bool(&self, address: Address) -> anyhow::Result<bool> {
+    pub fn get_bool(&self) -> anyhow::Result<bool> {
         todo!()
     }
 
-    pub fn get_int(&self, address: Address) -> anyhow::Result<i32> {
+    pub fn get_int(&self) -> anyhow::Result<i32> {
         todo!()
     }
 
-    pub fn get_any_ref(&self, address: Address) -> anyhow::Result<&dyn Any> {
+    pub fn get_any_ref(&self) -> anyhow::Result<&dyn Any> {
         todo!()
     }
 
-    pub fn get_any_mut(&self, address: Address) -> anyhow::Result<&mut dyn Any> {
+    pub fn get_any_mut(&mut self) -> anyhow::Result<&mut dyn Any> {
         todo!()
     }
 }
